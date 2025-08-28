6469c5ed58ddad116c3c8445afc087fa11a20314
justinasBaskys <justinas.baskys@gmail.com>
2025-08-28 17:02:10 +0300

    Revert "webpack error fixing"
    This reverts commit 250cf70edaf5673c69abb5a9de1c14c4c352449d.

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-import axios from "axios";
-import { CompanySearch } from "./company";
+import axios from "axios"
+import {CompanySearch} from "./company";
 
 interface SearchResponse {
     data: CompanySearch[];
@@ -8,23 +7,18 @@ interface SearchResponse {
 
 export const searchCompanies = async (query: string) => {
     try {
-        const response = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+        const data = await axios.get<SearchResponse>(
+        `https://financialmodelingprep.com/stable/search-name?query=AA&apikey=${process.env.REACT_APP_API_KEY}`
         );
-        return response.data;
-    } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
+        return data;
+    } catch (error) {
+        if(axios.isAxiosError(error)) {
             console.log("error message: ", error.message);
             return error.message;
-        } else {
-            console.log("unexpected error: ", error);
-            return "An unexpected error has occurred.";
         }
-=======
-import axios from "axios"
-export const searchCompanies = async (query: string) => {
-    try {
-        const data = await axios.get<>
->>>>>>> parent of 5b4684d (save before --force)
+        else {
+            console.log("unexpected error: ", error);
+            return "An unexpected error has occured.";
+        }
     }
-};
+}
