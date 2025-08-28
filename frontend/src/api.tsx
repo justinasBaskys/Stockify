9c1945301c4c18765d41603eab6671af0e1f39f5
justinasBaskys <justinas.baskys@gmail.com>
2025-08-28 22:14:57 +0300

    Added server disconnect handling

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -8,7 +8,7 @@ interface SearchResponse {
 export const searchCompanies = async (query: string) => {
     try {
         const data = await axios.get<SearchResponse>(
-        `https://financialmodelingprep.com/stable/search-name?query=AA&apikey=${process.env.REACT_APP_API_KEY}`
+        `https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
         );
         return data;
     } catch (error) {
