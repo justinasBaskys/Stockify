250cf70edaf5673c69abb5a9de1c14c4c352449d
justinasBaskys <justinas.baskys@gmail.com>
2025-08-28 16:30:02 +0300

    webpack error fixing

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,6 @@
-import axios from "axios"
-import {CompanySearch} from "./company";
+<<<<<<< HEAD
+import axios from "axios";
+import { CompanySearch } from "./company";
 
 interface SearchResponse {
     data: CompanySearch[];
@@ -7,18 +8,23 @@ interface SearchResponse {
 
 export const searchCompanies = async (query: string) => {
     try {
-        const data = await axios.get<SearchResponse>(
-        `https://financialmodelingprep.com/stable/search-name?query=AA&apikey=${process.env.REACT_APP_API_KEY}`
+        const response = await axios.get<SearchResponse>(
+            `https://financialmodelingprep.com/stable/search-name?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
         );
-        return data;
-    } catch (error) {
-        if(axios.isAxiosError(error)) {
+        return response.data;
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
             console.log("error message: ", error.message);
             return error.message;
-        }
-        else {
+        } else {
             console.log("unexpected error: ", error);
-            return "An unexpected error has occured.";
+            return "An unexpected error has occurred.";
         }
+=======
+import axios from "axios"
+export const searchCompanies = async (query: string) => {
+    try {
+        const data = await axios.get<>
+>>>>>>> parent of 5b4684d (save before --force)
     }
-}
+};
