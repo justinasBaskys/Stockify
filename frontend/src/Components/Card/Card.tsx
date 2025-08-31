8e0525efa6fc42b916982391fefaf1341f831ae0
justinasBaskys <justinas.baskys@gmail.com>
2025-08-31 21:03:22 +0300

    Tailwind install

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, {SyntheticEvent} from "react";
 import "./Card.css";
 import {CompanySearch} from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio";
 
 interface Props {
     id: string;
     searchResult: CompanySearch;
-
+    onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ id, searchResult })=> {
+const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate})=> {
 return (
-    <div className="card">
+    <div key={id} id={id} className="card">
         <h2>Stockify</h2>
         <div className="details">
             <h3>{searchResult.name} ({searchResult.symbol})</h3>
             <p>{searchResult.currency}</p>
         </div>
-            <p className="info">{searchResult.exchangeShortName} - {searchResult.stockExchange}</p>
+            <p className="info">
+                {searchResult.exchangeShortName} - {searchResult.stockExchange}
+            </p>
+            <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
     </div>
 )
 };
