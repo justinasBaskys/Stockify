ff805775c40d629c040b950f7fbdf11a114d0596
justinasBaskys <justinas.baskys@gmail.com>
2025-08-31 22:30:54 +0300

    Tailwind needs fixing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import React, {ChangeEvent, SyntheticEvent, useState} from "react";
 import {CompanySearch} from "./company";
 import {searchCompanies} from "./api";
 import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
 
@@ -48,6 +50,8 @@ function App() {
 
     return (
     <div className="App">
+        <Navbar />
+        <Hero />
         <Search
             onSearchSubmit={onSearchSubmit}
             search={search}
