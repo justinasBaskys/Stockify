2d94cbc0b64c3c91b830f1e3d9864f21200cdcea
justinasBaskys <justinas.baskys@gmail.com>
2025-09-03 14:52:06 +0300

    tailwind fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -51,6 +51,7 @@ function App() {
     return (
     <div className="App">
         <Navbar />
+        <h1 className="text-4xl font-bold text-red-500">Hello there traveller</h1>
         <Hero />
         <Search
             onSearchSubmit={onSearchSubmit}
