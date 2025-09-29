757a382dab700f8f897eea224fc75c101f0c0a5b
justinasBaskys <justinas.baskys@gmail.com>
2025-09-29 22:21:46 +0300

    CRUDelete implemented

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856a5eaaee6704c8f2116a81fb45b118f704387f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eadfe21bbaefe953e70cb57fd16ee8f87934c698")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
