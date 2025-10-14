0c01b845bfb95b71af3cebc47b223642e358f062
justinasBaskys <justinas.baskys@gmail.com>
2025-10-14 14:36:22 +0300

    Login

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0461a8b752326ae1fdaad2e39345dc3e44575652")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea55e84529d0809ebda99de16719e929e0299fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
