3911928c6ba24f3952be2ad4143e242b614705dd
justinasBaskys <justinas.baskys@gmail.com>
2025-10-21 14:06:53 +0300

    Portfolio Create

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fbcbf7594c6c65c54ecad45e020c1b00e9201b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8072539f407c0dd5105abf51edebec1382ef79f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
