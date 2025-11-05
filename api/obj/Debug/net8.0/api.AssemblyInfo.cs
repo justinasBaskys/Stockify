76fa8aaaa12728435356e84439e0a94cac8b6e7f
justinasBaskys <justinas.baskys@gmail.com>
2025-11-05 14:34:01 +0200

    Login to API from frontend

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdca05a6cf7661d440b4157b8839a7b7a62781a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b966845787ad1693a53e040f166e56b637365789")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
